d1eaf3037fe876bf357c504e8a2cd8897e1be71b
CrisTafRos <cristian.tafolla.rosales@gmail.com>
2019-05-23 10:58:50 -0500

    ADD: archivos

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="UTF-8">
+ <title>Examen2</title>	
+</head>
+<body>
+	<h1>Muerte en el Nilo</h1>
+	<br>
+	<h2>Agatha Christie</h2>
+        <button onclick=>Ver texto demo</button> 
+
+</body>
+</html>
+
+
